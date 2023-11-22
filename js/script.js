997f7d1c9320ca82e085f8a3fc927e7288ba3bb2
Nikolai <107766413+demonzloebychi@users.noreply.github.com>
2023-11-22 21:30:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ let isMobile = {
     Android: function() {return navigator.userAgent.match(/Android/i);},
     iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
     Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
-
+    any: function() {return (isMobile.Android() || isMobile.iOS() || isMobile.Opera());},
 
 };
+    let body = document.querySelector('body');
+if(isMobile.any()){
+    body.classList.add('touch');
+}else{
+    body.classList.add('mouse');
+}
