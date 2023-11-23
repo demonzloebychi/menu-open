486cda0a46f5260203aba08c8b28023bc3e51848
Nikolai <107766413+demonzloebychi@users.noreply.github.com>
2023-11-23 11:02:00 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-let isMobile = {
-    Android: function() {return navigator.userAgent.match(/Android/i);},
-    iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
-    Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
-    any: function() {return (isMobile.Android() || isMobile.iOS() || isMobile.Opera());},
+// let isMobile = {
+//     Android: function() {return navigator.userAgent.match(/Android/i);},
+//     iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+//     Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+//     any: function() {return (isMobile.Android() || isMobile.iOS() || isMobile.Opera());},
 
-};
+// };
     let body = document.querySelector('body');
-if(isMobile.any()){
     body.classList.add('touch');
-}else{
-    body.classList.add('mouse');
-}
+    let arrow = document.querySelectorAll('.arrow');
+for(i=0;i<arrow.length; i++){
+    let thisLink = arrow[i].previousElementSibling;
+    let subMenu = arrow[i].nextElementSibling;
+    let thisArrow = arrow[i];
+
+    thisLink.classList.add('parent');
+  arrow[i].addEventListener('click', function(){
+    subMenu.classList.toggle('open');
+    thisArrow.classList.toggle('active');
+  });
+};
